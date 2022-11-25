da5531d14a612cc631c4e2077be9e34dc7847324
Pietro Bucker <pietro_177@hotmail.com>
2022-11-25 14:55:02 -0300

    requisitos 1 2 3 feito

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Title from './Title';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <Title headline="teste" />
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
